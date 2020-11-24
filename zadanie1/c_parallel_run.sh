8e7bce4cebf163bcfebd2c2fd8e383e9a75e33c5
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-11-24 22:19:36 +0100

    [zad1] dodane skrypty do testowania

--- zadanie1/c_parallel_run.sh
+++ zadanie1/c_parallel_run.sh
@@ -1,2 +1,2 @@
-cp serial_260.txt serial.txt
-mpirun -np 2 ./a.out 260 64 1901 100.0 0.1 200
+cp serial_2000.txt serial.txt
+mpirun -np 6 ./a.out 2000 64 1901 100.0 0.1 200
--- zadanie1/c_serial_run.sh
+++ zadanie1/c_serial_run.sh
@@ -1,2 +1,2 @@
-mpirun -np 1 ./a.out 260 64 1901 100.0 0.1 200
-mv serial.txt serial_260.txt
+mpirun -np 1 ./a.out 2000 64 1901 100.0 0.1 200
+mv serial.txt serial_2000.txt
