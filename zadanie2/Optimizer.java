82c1bc533ea4dc6e4a6ea4fdaf037cc7be18d62b
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-11-27 21:57:51 +0100

    [zadanie2] początek prac nad Optimizerem

--- zadanie2/Optimizer.java
+++ zadanie2/Optimizer.java
@@ -1,9 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Optional;
+
 public class Optimizer implements OptimizerInterface {
-    BoardInterface board;
+    private BoardInterface board;
+    private int boardSize;
+    private HashSet<PawnInterface> pawns;
+    private HashMap<Integer, Thread> threads;
 
     @Override
     public void setBoard(BoardInterface board) {
         this.board = board;
+        boardSize = board.getSize();
+        initialize();
     }
 
     @Override
@@ -14,5 +25,27 @@ public class Optimizer implements OptimizerInterface {
     @Override
     public void resume() {
 
+    }
+
+    private void initialize() {
+        getPawnsInterfaces();
+        createThreadsForPawns();
+    }
+
+    private void getPawnsInterfaces() {
+        for (int col = 0; col < boardSize; col++) {
+            for (int row = 0; row < boardSize; row++) {
+                board.get(col, row).ifPresent(pawn -> pawns.add(pawn));
+            }
+        }
+    }
+
+    private void createThreadsForPawns() {
+        pawns.forEach(pawn -> {
+            int pawnId = pawn.getID();
+            threads.put(pawnId, new Thread(() -> {
+                System.out.printf("Tutej pionek nr %d%n", pawnId);
+            }));
+        });
     }
 }
