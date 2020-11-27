f8ac09fc9c9e861700b02cdab25b07931888b8b0
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-11-27 18:38:13 +0100

    [zadanie2] poprawione formatowanie nierozwiązanego zadania

--- zadanie2/OptimizerInterface.java
+++ zadanie2/OptimizerInterface.java
@@ -1,22 +1,22 @@
 
 public interface OptimizerInterface {
-	
-	/**
-	 * Metoda umożliwia przekazanie planszy z pionkami
-	 * 
-	 * @param board plansza
-	 */
-	public void setBoard( BoardInterface board );
-	
-	/**
-	 * Wstrzymanie pracy wątków. Po zakończeniu metody 
-	 * żaden z pionków nie może być w tracie zmiany położenia.
-	 */
-	public void suspend();
-	
-	/**
-	 * Kontynuacja pracy wątkow.
-	 */
-	public void resume();
+
+    /**
+     * Metoda umożliwia przekazanie planszy z pionkami
+     *
+     * @param board plansza
+     */
+    public void setBoard(BoardInterface board);
+
+    /**
+     * Wstrzymanie pracy wątków. Po zakończeniu metody
+     * żaden z pionków nie może być w tracie zmiany położenia.
+     */
+    public void suspend();
+
+    /**
+     * Kontynuacja pracy wątkow.
+     */
+    public void resume();
 
 }
