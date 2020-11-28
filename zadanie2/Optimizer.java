cc335da10168c8d960e1e973ef814a91eba339e2
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-11-28 15:29:38 +0100

    [zadanie 2] implementacja inicjalizacji pionków

--- zadanie2/Optimizer.java
+++ zadanie2/Optimizer.java
@@ -1,14 +1,11 @@
-import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Optional;
 
 public class Optimizer implements OptimizerInterface {
+    private final HashSet<PawnInterface> pawns = new HashSet<>();
+    private final HashMap<Integer, Thread> threads = new HashMap<>();
     private BoardInterface board;
     private int boardSize;
-    private HashSet<PawnInterface> pawns;
-    private HashMap<Integer, Thread> threads;
 
     @Override
     public void setBoard(BoardInterface board) {
@@ -29,23 +26,43 @@ public class Optimizer implements OptimizerInterface {
 
     private void initialize() {
         getPawnsInterfaces();
-        createThreadsForPawns();
+        createAndRegisterThreadsForPawns();
+        runAllPawnsThreads();
+        joinAllPawnsThreads();
+    }
+
+    private void joinAllPawnsThreads() {
+        threads.forEach((id, thread) -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void runAllPawnsThreads() {
+        threads.forEach((id, thread) -> thread.start());
     }
 
     private void getPawnsInterfaces() {
         for (int col = 0; col < boardSize; col++) {
             for (int row = 0; row < boardSize; row++) {
-                board.get(col, row).ifPresent(pawn -> pawns.add(pawn));
+                board.get(col, row).ifPresent(pawns::add);
             }
         }
     }
 
-    private void createThreadsForPawns() {
+    private void threadFunction(int pawnId) {
+        System.out.printf("Tutaj pionek nr %d\n", pawnId);
+    }
+
+    private void createAndRegisterThreadsForPawns() {
         pawns.forEach(pawn -> {
             int pawnId = pawn.getID();
-            threads.put(pawnId, new Thread(() -> {
-                System.out.printf("Tutej pionek nr %d%n", pawnId);
-            }));
+            Thread thread = new Thread(() -> threadFunction(pawnId));
+            pawn.registerThread(thread);
+            threads.put(pawnId, thread);
         });
     }
 }
